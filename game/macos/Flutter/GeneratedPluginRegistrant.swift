3c5510035ee4238f3ee2886d273674f4330470b7
nyto9999m <nyto9999@gmail.com>
2023-03-09 00:54:00 +0800

    circle position moved by user tap

--- game/macos/Flutter/GeneratedPluginRegistrant.swift
+++ game/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_crashlytics
+import games_services
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  GamesServicesPlugin.register(with: registry.registrar(forPlugin: "GamesServicesPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
