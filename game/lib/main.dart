9d47a83cdb64981608769882c0d402c9b36964db
Yuhsuan, Chen <nyto9999@gmail.com>
2023-03-09 19:04:04 +0800

    push

--- game/lib/main.dart
+++ game/lib/main.dart
@@ -1,13 +1,12 @@
 import 'dart:async';
-import 'dart:ffi';
-import 'dart:js_util';
-import 'package:flutter/gestures.dart';
-import 'package:flame/components.dart';
+import 'dart:math';
 
+import 'package:flame/components.dart';
 import 'package:flame/experimental.dart';
-import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+
+import 'drag_events_game.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,57 +14,18 @@ void main() async {
   runApp(GameWidget(game: MyGame()));
 }
 
-class MyGame extends FlameGame with HasDraggableComponents {
-  @override
-  Future<void> onLoad() async {
-    await super.onLoad();
-  }
+class MyCrate extends SpriteComponent {
+  MyCrate() : super(size: Vector2.all(16));
 
   @override
-  void onDragEnd(DragEndEvent event) {
-    super.onDragEnd(event);
-    if (!event.handled) {
-      print('Drag ended at ${event.toString()}');
-    }
+  Future<void> onLoad() async {
+    sprite = await Sprite.load('sprite.png');
   }
 }
 
-class Star extends PositionComponent with DragCallbacks {
-  final Color color;
-  final Paint _paint = Paint();
-  final Paint _borderPaint = Paint()
-    ..color = const Color(0xFF000000)
-    ..style = PaintingStyle.stroke
-    ..strokeWidth = 2.0;
-
-  final _shadowPaint = Paint()
-    ..color = const Color(0x44000000)
-    ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 4.0);
-
-  late final Path _path;
-  bool _isDragged = false;
-
+class MyGame extends FlameGame {
   @override
-  bool containsLocalPoint(Vector2 point) {
-    return super.containsLocalPoint(point);
-  }
-
-  @override
-  void render(Canvas canvas) {
-    if (_isDragged) {
-      _paint.color = color.withOpacity(0.5);
-      canvas.drawPath(_path, _shadowPaint);
-      canvas.drawPath(_path, _paint);
-    } else {
-      _paint.color = color;
-      canvas.drawPath(_path, _paint);
-      canvas.drawPath(_path, _borderPaint);
-    }
-  }
-
-  @override
-  void onDragStart(DragStartEvent event) {
-    _isDragged = true;
-    super.onDragStart(event);
+  Future<void> onLoad() async {
+    await add(MyCrate());
   }
 }
