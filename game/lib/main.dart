39691150535cf4880408381d6de5b8b15877f6ae
yuhsuan <nyto9999@gmail.com>
2023-03-09 15:36:57 +0800

    dragcompnent

--- game/lib/main.dart
+++ game/lib/main.dart
@@ -1,46 +1,71 @@
+import 'dart:async';
+import 'dart:ffi';
+import 'dart:js_util';
+import 'package:flutter/gestures.dart';
 import 'package:flame/components.dart';
+
 import 'package:flame/experimental.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(GameWidget(game: MyGame()));
 }
 
-//create my game calss which has CanvasTouchDetector and onPanUpdate and onTapDown methods
-class MyGame extends FlameGame with HasTappableComponents {
-  MyGame() {
-    add(MyCircle());
+class MyGame extends FlameGame with HasDraggableComponents {
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
   }
 
-  var touchPoint = Vector2.zero();
-
   @override
-  void onTapDown(TapDownEvent event) {
-    super.onTapDown(event);
+  void onDragEnd(DragEndEvent event) {
+    super.onDragEnd(event);
     if (!event.handled) {
-      touchPoint = event.canvasPosition;
+      print('Drag ended at ${event.toString()}');
     }
   }
 }
 
-// create a circle which position update by touchPoint
-class MyCircle extends PositionComponent with HasGameRef<MyGame> {
-  MyCircle() {
-    size = Vector2.all(100);
-    anchor = Anchor.center;
+class Star extends PositionComponent with DragCallbacks {
+  final Color color;
+  final Paint _paint = Paint();
+  final Paint _borderPaint = Paint()
+    ..color = const Color(0xFF000000)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 2.0;
+
+  final _shadowPaint = Paint()
+    ..color = const Color(0x44000000)
+    ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 4.0);
+
+  late final Path _path;
+  bool _isDragged = false;
+
+  @override
+  bool containsLocalPoint(Vector2 point) {
+    return super.containsLocalPoint(point);
   }
 
   @override
   void render(Canvas canvas) {
-    super.render(canvas);
-
-    canvas.drawCircle(Offset.zero, 50, Paint()..color = Colors.red);
+    if (_isDragged) {
+      _paint.color = color.withOpacity(0.5);
+      canvas.drawPath(_path, _shadowPaint);
+      canvas.drawPath(_path, _paint);
+    } else {
+      _paint.color = color;
+      canvas.drawPath(_path, _paint);
+      canvas.drawPath(_path, _borderPaint);
+    }
   }
 
   @override
-  void update(double dt) {
-    super.update(dt);
-    position = gameRef.touchPoint;
+  void onDragStart(DragStartEvent event) {
+    _isDragged = true;
+    super.onDragStart(event);
   }
 }
