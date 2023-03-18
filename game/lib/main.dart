f534b246b156a88b0c82dbf271a213c2bcc2cd15
nyto9999m <nyto9999@gmail.com>
2023-03-18 16:32:07 +0800

    fdsfsss

--- game/lib/main.dart
+++ game/lib/main.dart
@@ -1,31 +1,9 @@
-import 'dart:async';
-import 'dart:math';
-
-import 'package:flame/components.dart';
-import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
 
-import 'drag_events_game.dart';
+import 'package:flutter/material.dart';
+import 'package:game/drag_events_game.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(GameWidget(game: MyGame()));
-}
-
-class MyCrate extends SpriteComponent {
-  MyCrate() : super(size: Vector2.all(16));
-
-  @override
-  Future<void> onLoad() async {
-    sprite = await Sprite.load('sprite.png');
-  }
-}
-
-class MyGame extends FlameGame {
-  @override
-  Future<void> onLoad() async {
-    await add(MyCrate());
-  }
+  runApp(GameWidget(game: DragEventsGame()));
 }
